283c49d4617adc331901c13110772b2e9f8f2755
ManuelMannhold <m.mannhold@gmx.de>
2025-08-04 11:53:20 +0200

    add components

--- private-manager/src/app/app.routes.ts
+++ private-manager/src/app/app.routes.ts
@@ -1,3 +1,5 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { Main } from './main/main';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: Main }];
