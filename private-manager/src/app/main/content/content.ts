2fbd7588596e1925cef6ba531edeb14a52746050
ManuelMannhold <m.mannhold@gmx.de>
2025-08-11 11:57:25 +0200

    add route

--- private-manager/src/app/main/content/content.ts
+++ private-manager/src/app/main/content/content.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-content',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './content.html',
-  styleUrl: './content.scss'
+  styleUrl: './content.scss',
 })
-export class Content {
-
-}
+export class Content {}
