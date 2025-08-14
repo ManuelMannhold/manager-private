d937a26150798041b449a4fc435b44c0301ee595
ManuelMannhold <m.mannhold@gmx.de>
2025-08-14 11:42:49 +0200

    change navbar width

--- private-manager/src/app/app.routes.ts
+++ private-manager/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
-import { Main } from './main/main';
 import { Home } from './home/home';
 import { Garden } from './garden/garden';
 import { GardenHaveThere } from './garden/have-there/have-there';
@@ -8,7 +7,7 @@ import { GardenNotices } from './garden/notices/notices';
 import { GardenShopping } from './garden/shopping/shopping';
 
 export const routes: Routes = [
-  { path: '', component: Main },
+  { path: '', component: Garden },
   { path: 'home', component: Home },
   { path: 'garden', component: Garden },
   { path: 'garden-have', component: GardenHaveThere },
