3e4e1999f83d1c58e4ab0d15a75761dbefae22ce
ManuelMannhold <m.mannhold@gmx.de>
2025-08-19 11:33:07 +0200

    change style button

--- private-manager/src/app/app.ts
+++ private-manager/src/app/app.ts
@@ -5,6 +5,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [Sidebar, Navbar, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
