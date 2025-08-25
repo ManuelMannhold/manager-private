d927115d8c2ae2bd87de0a685188cc37b2f6f037
ManuelMannhold <m.mannhold@gmx.de>
2025-08-25 11:45:40 +0200

    add routerLink path

--- private-manager/src/app/app.routes.ts
+++ private-manager/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { Garden } from './garden/garden';
 import { GardenHaveThere } from './garden/have-there/have-there';
 import { GardenNotices } from './garden/notices/notices';
 import { GardenShopping } from './garden/shopping/shopping';
+import { HaveThere } from './home/have-there/have-there';
+import { Notices } from './home/notices/notices';
+import { Shopping } from './home/shopping/shopping';
 
 export const routes: Routes = [
   { path: '', component: GardenNotices },
@@ -13,5 +16,7 @@ export const routes: Routes = [
   { path: 'garden-have', component: GardenHaveThere },
   { path: 'garden-notices', component: GardenNotices },
   { path: 'garden-shopping', component: GardenShopping },
-  { path: 'garden', component: Garden },
+  { path: 'home-have', component: HaveThere },
+  { path: 'home-notices', component: Notices },
+  { path: 'home-shopping', component: Shopping },
 ];
